70c331f2560ba3f23023d96a99546378645a2e47
vavikast <vavikast@gmail.com>
2018-03-23 00:08:18 +0800

    add exception

--- py/class2/class9.py
+++ py/class2/class9.py
@@ -1,10 +1,16 @@
 #-*- coding: utf-8  -*-
-class Fib(object):
-	def __getitem__(self,n):
-		a,b=1,1
-		for x in range(n):
-			a,b=b,a+b
-		return a
-f=Fib()
-f[0]
-f[1]
+
+def foo(s):
+    n = int(s)
+    if n==0:
+        raise ValueError('invalid value: %s' % s)
+    return 10 / n
+
+def bar():
+    try:
+        foo(0)
+    except ValueError as e:
+        print('ValueError!')
+        raise
+
+bar()
