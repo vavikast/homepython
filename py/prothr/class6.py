5dcb9737ebba6cf4f1b80fb948af3e87d5b59b73
vavikast <vavikast@gmail.com>
2018-05-03 00:09:33 +0800

    add zhengze

--- py/prothr/class6.py
+++ py/prothr/class6.py
@@ -1,15 +1,3 @@
 #-*- coding: utf-8  -*-
-def foo(s):
-    return 10 / int(s)
+import re
 
-def bar(s):
-    return foo(s) * 2
-
-def main():
-    try:
-        bar('0')
-    except Exception as e:
-        print('Error:', e)
-    finally:
-        print('finally...')
-main()
