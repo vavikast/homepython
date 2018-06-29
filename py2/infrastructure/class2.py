82dc31e24c03202f725483e5661b6df7bb5d6094
vavikast <vavikast@gmail.com>
2018-06-29 18:24:51 +0800

    infruscture

--- py2/infrastructure/class2.py
+++ py2/infrastructure/class2.py
@@ -1,6 +1,15 @@
 # -*- coding: utf-8 -*-
-def now():
-	print('2015-3-25')
-f=now
-print(f())
-	
+def make_counter():
+    count = 0
+    def counter():
+        nonlocal count
+        count += 1
+        return count
+    return counter
+    
+def make_counter_test():
+  mc = make_counter()
+  print(mc())
+  print(mc())
+  print(mc())
+print(global_counter_test())
