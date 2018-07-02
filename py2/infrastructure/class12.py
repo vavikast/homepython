b9447d042aa1e16e325d4bcbf24017b37ad15ea8
vavikast <vavikast@gmail.com>
2018-07-02 17:46:25 +0800

    add infrustruct

---
+++ py2/infrastructure/class12.py
@@ -0,0 +1,9 @@
+#-*- coding: utf-8  -*-
+class MyDog(object):
+	def __len__(self):
+		self.x=9
+	def power(self)
+		return self.x*self.x
+		
+obj=MyObject()
+
---
+++ py2/infrastructure/class13.py
@@ -0,0 +1,8 @@
+#! -*- coding: utf-8 -*-
+class Student(object):
+	def __init__(self,name)
+		self.name = name
+		
+s=Student('Bob')
+s.score=90
+
